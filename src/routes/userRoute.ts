b7f186d595362e8b1eca84574c7dd7a1f0bb699d
Daniel-Ramirez <dramirez@arquesoft.com>
2023-10-19 16:53:45 -0500

    métodos route para usuarios

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 
 let router = express.Router();
 
-router.post('/',async (req: Request, res: Response) => {
+router.post('/addUser',async (req: Request, res: Response) => {
   try {
     const { name, email } = req.body;
     const user = await prisma.user.create({
@@ -22,11 +22,76 @@ router.post('/',async (req: Request, res: Response) => {
 });
 
 
-router.get("/",async (req: Request, res: Response) => {
+router.get("/getUsers",async (req: Request, res: Response) => {
     const users = await prisma.user.findMany()
     res.json(users)
 })
 
+router.post("/getUser",async (req: Request, res: Response) => {
+    
+  try {
+    const { id } = req.body;
+      // Get one User    
+    const user = await prisma.user.findUnique({
+      where: {        
+        id: id,
+      },
+    });
+
+    res.json(user)
+
+  }catch (error) {
+    console.error('Error getting user:', error);
+    res.status(500).json({ error: 'Error getting user' });
+  }  
+  
+})
+
+
+router.patch("/updateUser",async(req: Request, res: Response)=>{
+  
+  try{
+    const { id , name, email} = req.body;
+    const updateUser = await prisma.user.update({
+      where:{
+        id:id,
+      },
+      data:{
+        name: name,
+        email: email,
+      },
+    })
+
+    res.json("user with id: " + id+ " was updated ");
+
+  }
+  catch(error){
+    console.error('Error updating user:', error);
+    res.status(500).json({ error: 'Error updating user' });
+  }
+
+})
+
+
+router.delete("/deleteUser",async(req: Request, res: Response)=>{
+  
+  try{
+    const { id } = req.body;
+    const deleteUser = await prisma.user.delete({
+      where:{
+        id:id,
+      },      
+    })
+
+    res.json("user with id: " + id + " was deleted");
+
+  }
+  catch(error){
+    console.error('Error deleting user:', error);
+    res.status(500).json({ error: 'Error deleting user' });
+  }
+
+})
 
 module.exports = router;
 
